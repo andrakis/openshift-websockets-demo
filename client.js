e12891b81c388e6d101b5cc1216f30f8a97aad31
Julian Thatcher <julian.thatcher@freestyletechnology.com.au>
2016-05-24 15:30:54 +1000

    Update client.js for more features

--- client.js
+++ client.js
@@ -1,8 +1,23 @@
-var WebSocket = require('ws')
-, ws = new WebSocket('ws://websockets-julian.freestyle.com:8000');
-ws.on('open', function() {
+var WebSocket = require('ws'),
+    ws = new WebSocket('ws://websockets-julian.freestyle.com:8000');
+
+var messagesSent = 0;
+var maxMessages = 5;
+
+function sendSomething() {
 	ws.send('something');
+	messagesSent++;
+}
+
+ws.on('open', function() {
+	sendSomething();
 });
 ws.on('message', function(message) {
 	console.log('received: %s', message);
+	if(messagesSent <= maxMessages) {
+		sendSomething();
+	} else {
+		console.log("Max messages sent, disconnecting");
+		ws.close();
+	}
 });
